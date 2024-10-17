df35152d2b1c851b88554511c9db7354ca56d554
shobah24 <shokran.bahram14@gmail.se>
2024-10-17 18:18:39 +0200

    Add project files.

---
+++ Arean på en cirkel/Circle.cs
@@ -0,0 +1,19 @@
+﻿namespace Arean_på_en_cirkel
+{
+    internal class Circle
+    {
+        
+        float _pi = 3.141f;
+        int _Radius;
+
+        public Circle (int Radius)
+        {
+            _Radius = Radius;
+        }
+        public float GetArea() // float för att avrunda decimalerna
+        {
+            return _Radius * _Radius * _pi;
+           
+        }
+    }
+}
---
+++ Arean på en cirkel/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Arean_på_en_cirkel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arean med radien 5
+            Circle circle = new Circle(5);
+            Console.WriteLine($"Arean av en cirkel med radien 5 är: {circle.GetArea()}");
+            // Arean med radien 6
+            Circle circle2 = new Circle(6);
+            Console.WriteLine($"Arean av en cirkel med radien 6 är: {circle2.GetArea()}");
+
+        }
+    }
+}
